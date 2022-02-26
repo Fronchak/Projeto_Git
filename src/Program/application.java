8d276fcfaa088b1ae4c7dff29a3dfd06c4a92b8a
Gabriel Fronchak <gabrielgmack@gmail.com>
2022-02-26 16:17:35 -0300

    Created Project

---
+++ src/Program/application.java
@@ -0,0 +1,15 @@
+package Program;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+
+}
